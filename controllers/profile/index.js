3fce9b6e6bf5d64aa301ba3b8e08e7ccd929109a
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-04-01 10:25:32 -0700

    added spaces

--- controllers/profile/index.js
+++ controllers/profile/index.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { Profile } = require("../../models/profile");
 
-//get profiles
+// get profiles
 router.get("/", async (req, res) => {
   const profiles = await Profile.find();
   res.send(profiles);
   return profiles;
 });
 
-//get profile
+// get profile
 router.get("/profile/:id", function (req, res) {
   Profile.findById(id);
   res.send("Use controller");
 });
 
-//create profile
+// create profile
 router.post("/create", async (req, res) => {
   const profile = new Profile(req.body);
   await profile.save();
